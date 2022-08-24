019fc63ce9e2a31ec67478a56b349e46293b5960
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2022-08-23 23:18:29 -0300

    tests(models)

--- src/app/models/work-order.spec.ts
+++ src/app/models/work-order.spec.ts
@@ -1,7 +1,31 @@
-import { WorkOrder } from './work-order';
+import { WorkdOrderDto, WorkOrder, AssignedTo } from './work-order';
+
+const orderExample: WorkdOrderDto = {
+  work_order_id: 8,
+  description: 'Set up station for new user',
+  received_date: '2021-07-16 09:23:03',
+  assigned_to: [
+    {
+      person_name: 'Technician Two',
+      status: 'Completed',
+    },
+  ],
+  status: 'Completed',
+  priority: 'Low',
+};
 
 describe('WorkOrder', () => {
   it('should create an instance', () => {
-    expect(new WorkOrder()).toBeTruthy();
+    expect(new WorkOrder(orderExample)).toBeTruthy();
+  });
+
+  it('should convert string to date', () => {
+    const order = new WorkOrder(orderExample);
+    expect(order.receivedDate).toBeInstanceOf(Date);
+  });
+
+  it('should create instance of assignee', () => {
+    const order = new WorkOrder(orderExample);
+    expect(order.assignedTo[0]).toBeInstanceOf(AssignedTo);
   });
 });
