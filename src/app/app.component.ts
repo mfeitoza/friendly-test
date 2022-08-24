bbf60e28379c92638dbc7ba4034bb7a2bf8c2c54
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2022-08-23 23:10:37 -0300

    feat(app): implement table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { Observable, combineLatest } from 'rxjs';
+import { map, startWith, debounceTime } from 'rxjs/operators';
+
+import { Color } from './components/badge/badge.component';
+import {
+  Priority,
+  Status,
+  WorkdOrderDto,
+  WorkOrder,
+} from './models/work-order';
+import { ApiService } from './services/api.service';
+
+interface Country {
+  name: string;
+  flag: string;
+  area: number;
+  population: number;
+}
+
+function filterByDescription(order: WorkOrder, term: string) {
+  return order.description.toLowerCase().includes(term.toLowerCase());
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'friendly-test';
+  workOrders$: Observable<{ data: WorkOrder[]; isLoading: boolean }>;
+  filteredOrders$: Observable<WorkOrder[]>;
+  searchTerm = new FormControl('');
+
+  constructor(private api: ApiService) {
+    this.workOrders$ = this.api.getWorkOrders();
+    this.filteredOrders$ = combineLatest([
+      this.workOrders$,
+      this.searchTerm.valueChanges.pipe(startWith('')),
+    ]).pipe(
+      map(([{ data }, term]) => {
+        return data.filter((order) => filterByDescription(order, term));
+      })
+    );
+  }
+
+  getPriorityColor(priority: Priority) {
+    return {
+      Low: 'success',
+      Normal: 'primary',
+      High: 'danger',
+    }[priority] as Color;
+  }
+
+  getStatusColor(status: Status) {
+    return {
+      New: 'warning',
+      Confirmed: 'primary',
+      Canceled: 'danger',
+      Completed: 'success',
+    }[status] as Color;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BadgeComponent } from './components/badge/badge.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BadgeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
